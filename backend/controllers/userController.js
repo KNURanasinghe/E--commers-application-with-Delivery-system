085d6a47c28a42716282edd059fffbcf38a614b1
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2025-01-03 23:16:57 +0530

    user auth done

---
+++ backend/controllers/userController.js
@@ -0,0 +1,96 @@
+import validator from 'validator'
+import userModel from '../models/userModel.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const createToken= (id) =>{
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
+const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        const user = await userModel.findOne({email});
+
+        if(!user){
+            res.json({success:false, message:"User does not exist"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(isMatch){
+            const token = createToken(user._id)
+            res.json({success:true,token})
+        }
+        else{
+            res.json({success:false, message:"Invalid credentials"})
+
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})
+
+        
+    }
+}
+
+const registerUser = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        //checkin alredy exist
+        const exist =await userModel.findOne({email});
+        if (exist) {
+            return res.json({success: false, message:"User alredy exists"})
+        }
+
+        //valiate email and strong password
+        if (!validator.isEmail(email)) {
+            return res.json({success: false, message:"Please enter a valid email"})
+        }
+        if (password.length < 8) {
+            return res.json({success: false, message:"Please enter a strong password"})
+        }
+
+        //hash passwoord
+        const salt =  await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new userModel({
+            name,
+            email,
+            password:hashedPassword
+        })
+
+        const user = await newUser.save()
+
+        const token = createToken(user._id)
+
+        res.json({
+            success:true,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})        
+    }
+}
+
+const adminLogin= async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        console.log(re1.body);
+        
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password, process.env.JWT_SECRET);
+            res.json({success:true, token})
+        }else{
+            res.json({success:false, message:"invalid credentials"})
+        }
+    } catch (error) {
+        
+    }
+}
+
+export  {loginUser, registerUser, adminLogin}
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectDb from './config/mongodb.js';
+import connectCloudinary from './config/cloudnary.js';
+import userRouter from './routes/userRoute.js';
+import productRouter from './routes/product.route.js';
+
+//app config
+const app = express()
+const port = process.env.PORT || 4000
+connectDb() 
+connectCloudinary()
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+
+app.get('/',(req, res)=> {
+    res.send("api working")
+})
+
+app.listen(port, ()=> console.log('server strated on port: ' + port))
